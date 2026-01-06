ba63df3f74a377938446ee6aa2d627fb487cd267
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-01-06 16:38:45 +0530

    all data uploaded

--- module-5-laravel/what is laravel/first-demo-app/routes/web.php
+++ module-5-laravel/what is laravel/first-demo-app/routes/web.php
@@ -16,3 +16,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',function(){
  return view('address'); 
 });
+
+// print hello world direct return variables 
+Route::get('/demo',function(){
+//print hello world return a direct variables via routing  
+$name="Hello world";
+return "This will be print $name, Hi i am just load routing";
+
+});
+
+// return html in routing using php variables 
+
+Route::get('/contact-us',function(){
+
+    $name="Brijesh kumar pandey";
+    $mob=9998003879;
+    $address="150 feet ring road rajkot 360005";
+    return "<p>$name <br>  $mob <br> $address </p>";
+
+});
+
+// return a dd() and dump() for laravel debugging
+
+Route::get('/abc',function(){
+
+    $name=["brijesh","rajesh","kumar"];
+    dd($name); // print code and stop excecution 
+
+});
+
+// print data in blade via routing 
+
+Route::get('/template',function(){
+
+    $name="Meet kumar pandey";
+    return view('about-us',compact('name'));
+
+});
+
+// print data in blade via routing and return a ID 
+
+Route::get('/template/{id}',function($id){
+
+    return view('about-us',compact('id'));
+
+});
+
